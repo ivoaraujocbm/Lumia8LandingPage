94d3f31c769ba07cb117bf42c576ca20aa7b5c1f
ivoaraujocbm <ivo.araujo@cbminformatica.com.br>
2026-02-11 10:05:07 -0300

    Iniciando Projeto Landing Page Lumia8

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+  <div class="flex min-h-screen bg-slate-50">
+    <main class="flex-1 ml-64">
+      <div class="max-w-7x1 mx-auto">
+        Olá mundo
+      </div>
+    </main>
+  </div>
+</template>
