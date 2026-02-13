016e9464f62c9617d95a74a396d9a2235f27d425
ivoaraujocbm <ivo.araujo@cbminformatica.com.br>
2026-02-13 11:38:42 -0300

    Componente Hero finalizado.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
   <div class="min-h-screen bg-slate-50 text-slate-900 font-sans selection:bg-blue-100 selection:text-blue-600">
     <Navbar />
 
+    <Hero />
+
+    
+
   </div>
 </template>
 
 <script setup>
+import Hero from './components/Hero.vue';
 import Navbar from './components/Navbar.vue';
 
 </script>
